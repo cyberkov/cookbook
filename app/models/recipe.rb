3d08b154685e1161591bec543dac6965917887b0
Hannes Schaller <admin@cyberkov.at>
2013-01-24 18:02:39 +0100

    added nested from.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,7 +2,8 @@ class Recipe < ActiveRecord::Base
   has_and_belongs_to_many :ingredients
   has_and_belongs_to_many :tags
   has_many :lunchplans
-  attr_accessible :description, :name, :persons, :preparation_time, :tags, :ingredients
+  attr_accessible :description, :name, :persons, :preparation_time, :tags, :ingredients, :ingredients_attributes
+  accepts_nested_attributes_for :ingredients
 
   validate :name, :uniqueness => true
 end
