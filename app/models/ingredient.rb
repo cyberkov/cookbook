dc03d039a6b1bb3d36ee2561e61f4a84180e18c5
Hannes Schaller <admin@cyberkov.at>
2013-01-25 15:29:53 +0100

    changed seeds

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ActiveRecord::Base
-  attr_accessible :amount, :food_id, :recipe_id, :unit_id
+  attr_accessible :amount, :food_id, :recipe_id, :unit_id, :unit, :food
   belongs_to :recipe
   belongs_to :food
   belongs_to :unit
