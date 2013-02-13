bdafd7440bfdf19588e9c7a3130b93b191d435df
Hannes Schaller <admin@cyberkov.at>
2013-02-13 11:44:55 +0100

    moved back to normal forms

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,7 +4,7 @@ class Recipe < ActiveRecord::Base
   has_many :tags
   has_many :lunchplans
   attr_accessible :description, :name, :persons, :preparation_time, :tags, :ingredients, :ingredients_attributes
-  accepts_nested_attributes_for :ingredients
+  accepts_nested_attributes_for :ingredients, :allow_destroy => true
 
   validate :name, :uniqueness => true
   validates :name, :description, :persons, :ingredients, :preparation_time, :presence => true
