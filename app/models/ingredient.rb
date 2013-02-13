be68364db28e7f10e78fd5e5d54765e027009ed5
Hannes Schaller <admin@cyberkov.at>
2013-02-13 12:25:22 +0100

    added dependency

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,5 @@
 class Ingredient < ActiveRecord::Base
-  attr_accessible :amount, :food_id, :recipe_id, :unit_id, :unit, :food
+  attr_accessible :amount, :food_id, :recipe_id, :unit_id, :unit, :food, :food_name
   belongs_to :recipe
   belongs_to :food
   belongs_to :unit
@@ -7,4 +7,12 @@ class Ingredient < ActiveRecord::Base
   # A recipe cannot have more than one ingredient per food
   validates :food_id, :uniqueness => { :scope => :recipe_id }
   validates :amount, :presence => true
+
+  def food_name
+		  food.try(:name)
+  end
+
+  def food_name=(name)
+		  self.food = Food.find_or_create_by_name(name) if name.present?
+  end
 end
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,6 +1,6 @@
 class Unit < ActiveRecord::Base
   attr_accessible :name
-  has_many :ingredients
+  has_many :ingredients, :dependent => :restrict
   validates :name, :uniqueness => true
   validates :name, :presence => true
 end
